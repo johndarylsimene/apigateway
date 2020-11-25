e45302a01c9ae2fb52c686ca8ad33ca7825f2b49
[johndarylsimene] <[johndaryl.simene@gmail.com]>
2020-11-25 19:37:59 +0800

    First upload

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+// unsecure routes
+
+   // $router->get('/users',['uses' => 'UserController@getUsers']);
+$router->get('/login', 'UserController@login');
+$router->post('/validation', 'UserController@validation'); 
+$router->get('/users', 'UserController@show');
+$router->post('/users', 'UserController@addUser');  //ADDS USER
+$router->get('/users/{id}', 'UserController@index');   //FIND THE INPUT ID
+$router->put('/users/{id}', 'UserController@updateUser');   //UPDATE
+$router->patch('/users/{id}', 'UserController@updateUser');   //update user
+$router->delete('/users/{id}', 'UserController@removeUser');  //DELETES USER
+
+
+
+
+
